d5cf6e13ffa23d8913a557e8c32680ae6e514413
unknown <tta2yta@gmail.com>
2020-11-05 12:16:17 +0300

    adding featuters dbcontext class

---
+++ CourseLibraryAPI/DbContexts/CourseLibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using CourseLibraryAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibraryAPI.DbContexts
+{
+    public class CourseLibraryContext :DbContext
+    {
+        public CourseLibraryContext(DbContextOptions<CourseLibraryContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
