2ce18225ff864ee2acd12ca4d4975ce3c3009cc1
unknown <tta2yta@gmail.com>
2020-11-05 12:00:58 +0300

    adding author properties

--- CourseLibraryAPI/Entities/Author.cs
+++ CourseLibraryAPI/Entities/Author.cs
@@ -7,5 +7,15 @@ namespace CourseLibraryAPI.Entities
 {
     public class Author
     {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public string MainCategory { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+
+
     }
 }
