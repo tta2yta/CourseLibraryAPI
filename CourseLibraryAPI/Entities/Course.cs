1a2ba7a4239e42126d6b5ff0ce1bf9f7aef8e0d1
unknown <tta2yta@gmail.com>
2020-11-05 12:08:20 +0300

    adding course properties

--- CourseLibraryAPI/Entities/Course.cs
+++ CourseLibraryAPI/Entities/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,16 @@ namespace CourseLibraryAPI.Entities
 {
     public class Course
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey('AuthorId')]
+        public Author Author { get; set; }
+        public Guid AuthorId { get; set; }
     }
 }
