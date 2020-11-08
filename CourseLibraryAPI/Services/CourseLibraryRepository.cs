21fcf01b9db7c294d04799e577771a62391be56b
unknown <tta2yta@gmail.com>
2020-11-08 23:36:10 +0300

    adding GetCourses method

--- CourseLibraryAPI/Services/CourseLibraryRepository.cs
+++ CourseLibraryAPI/Services/CourseLibraryRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CourseLibraryAPI.DbContexts;
+using CourseLibraryAPI.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,26 @@ namespace CourseLibraryAPI.Services
 {
     public class CourseLibraryRepository
     {
+        private readonly CourseLibraryContext _context;
+
+        public CourseLibraryRepository(CourseLibraryContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Course GetCourse(Guid CourseId, Guid AuthorId)
+        {
+            if (CourseId == null)
+            {
+                throw new ArgumentNullException(nameof(CourseId));
+            }
+            if (AuthorId == null)
+            {
+                throw new ArgumentNullException(nameof(AuthorId));
+            }
+
+            return _context.Courses.Where(res => res.Id == AuthorId && res.Id == CourseId).FirstOrDefault();
+        }
+
     }
 }
