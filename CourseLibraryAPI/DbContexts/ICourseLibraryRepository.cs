5c89512e263d0c5c37daa4c0d7335d55f31b9fc2
unknown <tta2yta@gmail.com>
2020-11-16 15:00:51 +0300

    adding features to ICourseLibraryInterface

--- CourseLibraryAPI/DbContexts/ICourseLibraryRepository.cs
+++ CourseLibraryAPI/DbContexts/ICourseLibraryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseLibraryAPI.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace CourseLibraryAPI.DbContexts
 {
     interface ICourseLibraryRepository
     {
+        void AddCourse(Guid authorId, Course course);
+        void DeleteCourse(Course course);
+        Course GetCourse(Guid authorId, Guid courseId);
+        IEnumerable<Course> GetCourses(Guid authorId);
+        void UpdateCourse(Course course);
+        void AddAuthor(Author author);
+        bool AuthorExists(Guid authorId);
     }
 }
