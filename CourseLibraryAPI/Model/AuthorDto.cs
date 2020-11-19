ad3ca16bd4e829b564dcae596f9737fc118a331c
unknown <tta2yta@gmail.com>
2020-11-19 17:24:16 +0300

    modifying properties AuthorDto class

--- CourseLibraryAPI/Model/AuthorDto.cs
+++ CourseLibraryAPI/Model/AuthorDto.cs
@@ -8,9 +8,9 @@ namespace CourseLibraryAPI.Model
     public class AuthorDto
     {
         public Guid Guid { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTimeOffset DateofBirth { get; set; }
+        public string Name { get; set; }
+     
+        public int Age { get; set; }
         public string MainCategory { get; set; }
     }
 }
