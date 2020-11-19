1c7378313ce58bd47d838be8dfea0807f1627708
unknown <tta2yta@gmail.com>
2020-11-19 17:42:02 +0300

    adding GetCurrentAge method in DateTimeoffsetExtension

--- CourseLibraryAPI/Helpers/DateTimeOffsetExtension.cs
+++ CourseLibraryAPI/Helpers/DateTimeOffsetExtension.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace CourseLibraryAPI.Helpers
 {
-    public class DateTimeOffsetExtension
+    public static class DateTimeOffsetExtension
     {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var currentDate = DateTime.UtcNow;
+            int age = currentDate.Year - dateTimeOffset.Year;
+            if(currentDate < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+
     }
 }
