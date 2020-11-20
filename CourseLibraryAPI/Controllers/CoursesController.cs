9213de2007cd517275f3522acdde20f36a96820d
unknown <tta2yta@gmail.com>
2020-11-20 16:38:22 +0300

    adding features to CourseController

--- CourseLibraryAPI/Controllers/CoursesController.cs
+++ CourseLibraryAPI/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CourseLibraryAPI.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,15 @@ namespace CourseLibraryAPI.Controllers
 {
     public class CoursesController:ControllerBase
     {
-       
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository,
+           IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(
+                nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+        }
     }
 }
