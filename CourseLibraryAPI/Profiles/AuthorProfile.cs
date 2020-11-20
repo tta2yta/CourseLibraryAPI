d19e5e6ec6025af1c63faeac173f4d6450e9bf0d
unknown <tta2yta@gmail.com>
2020-11-20 16:18:17 +0300

    modifying AuthorProfile class

--- CourseLibraryAPI/Profiles/AuthorProfile.cs
+++ CourseLibraryAPI/Profiles/AuthorProfile.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using CourseLibraryAPI.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibraryAPI.Profiles
 {
-    public class AuthorProfile
+    public class AuthorProfile: Profile
     {
         public AuthorProfile()
         {
+            CreateMap<Entities.Author, Model.AuthorDto>()
+                .ForMember(
+                dest => dest.Name,
+                opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Age,
+                opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge())
+                );
 
         }
     }
