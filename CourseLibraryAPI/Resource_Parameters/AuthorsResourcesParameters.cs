6e05341d7f0c7fd4ce7eeb23dc1faf843db5bbef
unknown <tta2yta@gmail.com>
2020-11-24 17:24:03 +0300

    adding AUthorsResourcesParameters

---
+++ CourseLibraryAPI/Resource_Parameters/AuthorsResourcesParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibraryAPI.Resource_Parameters
+{
+    public class AuthorsResourcesParameters
+    {
+        public string MainCategory { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
