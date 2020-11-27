c0da9e7fc33b9ecc2be03d3bd8b108cbd47edb87
unknown <tta2yta@gmail.com>
2020-11-27 16:35:11 +0300

    adding properties to AuthorCreationDto

--- CourseLibraryAPI/Model/AuthorCreationDto.cs
+++ CourseLibraryAPI/Model/AuthorCreationDto.cs
@@ -7,5 +7,10 @@ namespace CourseLibraryAPI.Model
 {
     public class AuthorCreationDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string MainCategory { get; set; }
+
     }
 }
