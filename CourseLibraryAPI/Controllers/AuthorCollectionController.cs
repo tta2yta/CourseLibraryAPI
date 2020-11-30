8ad9c736c4c80ab4158d61b5cddc4de57b1fce7a
unknown <tta2yta@gmail.com>
2020-11-30 14:10:58 +0300

    adding features to authorcollectioncontroller

--- CourseLibraryAPI/Controllers/AuthorCollectionController.cs
+++ CourseLibraryAPI/Controllers/AuthorCollectionController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibraryAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseLibraryAPI.Controllers
 {
     public class AuthorCollectionController : Controller
     {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionController(ICourseLibraryRepository courseLibraryRepository, 
+            IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+        }
         public IActionResult Index()
         {
             return View();
